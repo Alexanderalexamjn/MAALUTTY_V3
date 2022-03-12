5b29bfcce33a5bbbdbb8e6b9bb88559cf855f68e
Alexanderalexamjn <98740540+Alexanderalexamjn@users.noreply.github.com>
2022-03-13 02:21:54 +0530

    Update attp.js

--- plugins/attp.js
+++ plugins/attp.js
@@ -1,4 +1,4 @@
-const Maalutty = require('../events');
+const Munavir = require('../events');
 const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
 const fs = require('fs');
 const axios = require('axios');
